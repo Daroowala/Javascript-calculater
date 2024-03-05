7df859e266b3cfc674c70e7ef48153e37b97e5f1
my name <blueshaperd3@gmail.com>
2024-03-06 00:57:29 +0500

    calculater

---
+++ app.js
@@ -0,0 +1,27 @@
+var valueOne = +prompt("Enter value one");
+var valueTwo = +prompt("Enter value two");
+var operator = prompt("Enter arithmetic operator")
+if (operator === "+"){
+console.log(valueOne + valueTwo)
+}
+
+var valueOne = +prompt("Enter value one");
+var valueTwo = +prompt("Enter value two");
+var operator = prompt("Enter airthmetic operator");
+if (operator === "-"){
+    alert(valueOne - valueTwo)
+}
+
+var valueOne = +prompt("Enter value one");
+var valueTwo = +prompt("Enter value two");
+var operator = prompt("Enter airthmetic operator");
+if (operator === "*"){
+    alert(valueOne * valueTwo)
+}
+
+var valueOne = +prompt("Enter value one");
+var valueTwo = +prompt("Enter value two");
+var operator = prompt("Enter airthmetic operator");
+if (operator === "/");{
+alert(valueOne / valueTwo)
+}
