0a914c1771166345950ecbf63b88197737e47e7e
my name <blueshaperd3@gmail.com>
2024-03-06 02:02:03 +0500

    age calculater

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-var valueOne = +prompt("Enter value one");
-var valueTwo = +prompt("Enter value two");
-var operator = prompt("Enter arithmetic operator")
-if (operator === "+"){
-console.log(valueOne + valueTwo)
-}
+//var valueOne = +prompt("Enter value one");
+//var valueTwo = +prompt("Enter value two");
+//var operator = prompt("Enter arithmetic operator")
+//if (operator === "+"){
+//console.log(valueOne + valueTwo);
+//}
 
-var valueOne = +prompt("Enter value one");
-var valueTwo = +prompt("Enter value two");
-var operator = prompt("Enter airthmetic operator");
-if (operator === "-"){
-    alert(valueOne - valueTwo)
-}
+//var valueOne = +prompt("Enter value one");
+//var valueTwo = +prompt("Enter value two");
+//var operator = prompt("Enter airthmetic operator");
+//if (operator === "-"){
+//console.log(valueOne - valueTwo);
+//}
 
-var valueOne = +prompt("Enter value one");
-var valueTwo = +prompt("Enter value two");
-var operator = prompt("Enter airthmetic operator");
-if (operator === "*"){
-    alert(valueOne * valueTwo)
-}
+//var valueOne = +prompt("Enter value one");
+//var valueTwo = +prompt("Enter value two");
+//var operator = prompt("Enter airthmetic operator");
+//if (operator === "*"){
+//console.log(valueOne * valueTwo);
+//}
 
-var valueOne = +prompt("Enter value one");
-var valueTwo = +prompt("Enter value two");
-var operator = prompt("Enter airthmetic operator");
-if (operator === "/");{
-alert(valueOne / valueTwo)
+//var valueOne = +prompt("Enter value one");
+//var valueTwo = +prompt("Enter value two");
+//var operator = prompt("Enter airthmetic operator");
+//if (operator === "/"){
+//console.log(valueOne / valueTwo);
+//}
+
+//else{
+  //  alert("Invaled operator, please use + - * /")
+//}
+
+var age = +prompt("Enter your age");
+
+if (age <= 17) {
+document.write("Ticket price is Rs 500")
+}
+if (age >= 18 && age < 50) {
+document.write("Ticket price is Rs 1000")
+}
+if (age >= 50) {
+  document.write("Free Entry")
 }
